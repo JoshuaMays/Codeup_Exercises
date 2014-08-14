db920549feacbccc30119b9c81aa3c842bb27ec4
haydogsup <joshua.r.mays@gmail.com>
2014-08-14 16:07:38 -0500

    second attempt, shortened for loop.

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,19 +1,35 @@
 <?php
 
-for ($i = 1; $i <= 100; $i++) {
-	if($i%3 == 0) {
-		echo "Fizz";
-		if($i%5 == 0) {
-			echo "Buzz\n";
-		} else {
-			echo "\n";
-		}
+// for ($i = 1; $i <= 100; $i++) {
+// 	if($i%3 == 0) {
+// 		echo "Fizz";
+// 		if($i%5 == 0) {
+// 			echo "Buzz\n";
+// 		} else {
+// 			echo "\n";
+// 		}
 
-	} elseif ($i%5 == 0) {
+// 	} elseif ($i%5 == 0) {
+// 		echo "Buzz\n";
+// 	} else {
+// 		echo $i . "\n";
+// 	}
+// }
+
+
+for ($i = 1; $i <=100; $i++) {
+	if ($i%3 == 0 && $i%5 == 0) {
+		echo "FizzBuzz\n";
+	} elseif ($i%3 == 0 && $i%5 != 0) {
+		echo "Fizz\n";
+	} elseif ($i%3 != 0 && $i%5 == 0) {
 		echo "Buzz\n";
 	} else {
-		echo $i . "\n";
+		echo "$i\n";
 	}
 }
+
+
+
 
 ?>
