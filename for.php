26b0b34691c272a0cf8a34e96af8c4664851bea2
haydogsup <joshua.r.mays@gmail.com>
2014-08-14 17:55:59 -0500

    added error message if input is non-numeric

--- for.php
+++ for.php
@@ -1,10 +1,25 @@
 <?php
 
+// Asking for and setting a lower bound.
 fwrite(STDOUT, "Please enter a starting number: ");
 $start = trim(fgets(STDIN));
 
+// Asking for and setting an upper bound.
 fwrite(STDOUT, "Now enter an ending number: ");
 $end = trim(fgets(STDIN));
+
+// Make sure that the upper and lower bounds are numbers
+// if not, print an error message.
+while (!is_numeric($start) || !is_numeric($end)) {
+	fwrite(STDOUT,"It doesn't look like you're giving me numbers.\n" 
+				. "Please try again.\n");
+	
+	fwrite(STDOUT, "Enter a starting number: ");
+	$start = trim(fgets(STDIN));
+
+	fwrite(STDOUT, "Now enter an ending number: ");
+	$end = trim(fgets(STDIN));
+}
 
 fwrite(STDOUT, "Enter an incrementer: ");
 $incrementer = trim(fgets(STDIN));
@@ -16,7 +31,5 @@ if (!$incrementer) {
 for ($i = $start; $i <= $end; $i+=$incrementer) {
 	fwrite(STDOUT,"$i\n");
 }
-
-
 
 ?>
