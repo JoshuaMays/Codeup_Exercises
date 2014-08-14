aab6746b2890a0f42f070fb3d24cd46f3dcf641a
haydogsup <joshua.r.mays@gmail.com>
2014-08-14 18:25:55 -0500

    Added ability to decrement if user inputs greater value first.

--- for.php
+++ for.php
@@ -8,28 +8,34 @@ $start = trim(fgets(STDIN));
 fwrite(STDOUT, "Now enter an ending number: ");
 $end = trim(fgets(STDIN));
 
+// Set $incrementer value to user input.
+fwrite(STDOUT, "Enter an incrementer: ");
+$incrementer = trim(fgets(STDIN));
 // Make sure that the upper and lower bounds are numbers
 // if not, print an error message.
 while (!is_numeric($start) || !is_numeric($end)) {
 	fwrite(STDOUT,"It doesn't look like you're giving me numbers.\n" 
 				. "Please try again.\n");
-	
+	// Reset the $start value to user input.
 	fwrite(STDOUT, "Enter a starting number: ");
 	$start = trim(fgets(STDIN));
-
+	// Reset the $end value to user input.
 	fwrite(STDOUT, "Now enter an ending number: ");
 	$end = trim(fgets(STDIN));
 }
 
-fwrite(STDOUT, "Enter an incrementer: ");
-$incrementer = trim(fgets(STDIN));
-
-if (!$incrementer) {
+// Set a $incrementer to default value (1) if user input is non-numeric.
+if (!is_numeric($incrementer)) {
 	$incrementer = 1;
 }
-
-for ($i = $start; $i <= $end; $i+=$incrementer) {
-	fwrite(STDOUT,"$i\n");
+// Check if program should be increment or decrement.
+if ($start <= $end) {
+	for ($i = $start; $i <= $end; $i+=$incrementer) {
+		fwrite(STDOUT,"$i\n");
+	}
+} else {
+	for ($i = $start; $i >= $end; $i-=$incrementer) {
+		fwrite(STDOUT, "$i\n");
+    }
 }
-
 ?>
