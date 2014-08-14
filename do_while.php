e76e230ce5e791a86987f65a4b388da4ae8cd329
haydogsup <joshua.r.mays@gmail.com>
2014-08-14 10:33:34 -0500

    added back older exercises. oops

--- do_while.php
+++ do_while.php
@@ -1,5 +1,24 @@
 <?php
 
+
+// Exercise 1
+$a = 0;
+do {
+	echo $a . "\n";
+	$a += 2;
+} while ($a <= 100);
+
+
+// Exercise 2
+$a = 100;
+
+do {
+	echo $a . "\n";
+	$a -= 5;
+} while ($a >= -10);
+
+
+// Exercise 3
 $a = 2;
 
 do {
