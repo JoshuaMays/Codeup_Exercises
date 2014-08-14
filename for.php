eaf6eb89bda41c16dc749ad415771a73c6b6dba1
haydogsup <joshua.r.mays@gmail.com>
2014-08-14 17:03:31 -0500

    Add user choice incrementer.

--- for.php
+++ for.php
@@ -2,10 +2,14 @@
 
 fwrite(STDOUT, "Please enter a starting number: ");
 $start = trim(fgets(STDIN));
+
 fwrite(STDOUT, "Now enter an ending number: ");
 $end = trim(fgets(STDIN));
 
-for ($i = $start; $i <= $end; $i++) {
+fwrite(STDOUT, "Enter an incrementer: ");
+$incrementer = trim(fgets(STDIN));
+
+for ($i = $start; $i <= $end; $i+=$incrementer) {
 	fwrite(STDOUT,"$i\n");
 }
 
