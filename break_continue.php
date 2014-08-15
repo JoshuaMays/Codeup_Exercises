16338c14378874238ffd9a23b6bb909015a0e083
haydogsup <joshua.r.mays@gmail.com>
2014-08-15 15:45:46 -0500

    break new (1-100) loop after reaches 10

--- break_continue.php
+++ break_continue.php
@@ -1,5 +1,20 @@
 <?php
 
+for ($i = 1; $i <=100; $i++) {
+	if ($i % 2 != 0) {
+		// Don't echo odd numbers, continue loop.
+		continue;
+	}
+	echo $i . PHP_EOL;
+}
+
+for ($i = 1; $i <= 100; $i++) {
+	echo $i . PHP_EOL;
+	if ($i == 10) {
+		// Quit looping when $i reaches 10.
+		break;
+	}
+}
 
 
 ?>
