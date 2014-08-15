5cc3a3a44d89ae12f5c67da406dafa5a557b457d
haydogsup <joshua.r.mays@gmail.com>
2014-08-15 10:22:46 -0500

    exercise 4

--- foreach.php
+++ foreach.php
@@ -18,17 +18,28 @@ $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, 
 // 	}
 // }
 
+// foreach ($things as $thing) {
+// 	if (is_scalar($thing)) {
+// 		if (is_float($thing)) {
+// 			echo "\$thing is a float.\n";
+// 		} elseif (is_int($thing)) {
+// 			echo "\$thing is an int.\n";
+// 		} elseif (is_string($thing)) {
+// 			echo "\$thing is a string.\n";
+// 		} elseif (is_bool($thing)) {
+// 			echo "\$thing is a boolean.\n";
+// 		}
+// 	}
+// }
+
 foreach ($things as $thing) {
-	if (is_scalar($thing)) {
-		if (is_float($thing)) {
-			echo "\$thing is a float.\n";
-		} elseif (is_int($thing)) {
-			echo "\$thing is an int.\n";
-		} elseif (is_string($thing)) {
-			echo "\$thing is a string.\n";
-		} elseif (is_bool($thing)) {
-			echo "\$thing is a boolean.\n";
+	if (is_array($thing)) {
+		foreach ($thing as $value) {
+			echo "{$value} ";
 		}
+		echo "\n";
+	} else {
+		echo "$thing\n";
 	}
 }
 
