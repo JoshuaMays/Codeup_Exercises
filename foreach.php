237df70891644968847ea4d5861d3bdbf7309ca6
haydogsup <joshua.r.mays@gmail.com>
2014-08-15 10:40:18 -0500

    formatted text

--- foreach.php
+++ foreach.php
@@ -2,6 +2,8 @@
 
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
+# ECHO text as data type.
+
 // foreach ($things as $thing) {
 // 	if (is_array($thing)) {
 // 		echo "\$thing is an array.\n";
@@ -18,6 +20,26 @@ $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, 
 // 	}
 // }
 
+# ECHO gettype($thing) to show data type of the values inside the $things array.
+
+foreach ($things as $thing) {
+	if (is_array($thing)){
+		echo "\$thing is an " . gettype($thing) . " data type.\n";
+	} elseif (is_bool($thing)) {
+		echo "\$thing is a " . gettype($thing) . " data type.\n";
+	} elseif (is_float($thing)) {
+		echo "\$thing is a " . gettype($thing) . " data type.\n";
+	} elseif (is_int($thing)) {
+		echo "\$thing is an " . gettype($thing) . " data type.\n";
+	} elseif (is_null($thing)) {
+		echo "\$thing is a " . gettype($thing) . " data type.\n";
+	} elseif (is_string($thing)) {
+		echo "\$thing is a " . gettype($thing) . " data type.\n";
+	}
+}
+
+# ECHO text to show data types for scalar values
+
 // foreach ($things as $thing) {
 // 	if (is_scalar($thing)) {
 // 		if (is_float($thing)) {
@@ -32,15 +54,17 @@ $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, 
 // 	}
 // }
 
-foreach ($things as $thing) {
-	if (is_array($thing)) {
-		foreach ($thing as $value) {
-			echo "{$value} ";
-		}
-		echo "\n";
-	} else {
-		echo "$thing\n";
-	}
-}
+# ECHO values, if value is an array, loop through the array and echo the stored values
+
+// foreach ($things as $thing) {
+// 	if (is_array($thing)) {
+// 		foreach ($thing as $value) {
+// 			echo "{$value} ";
+// 		}
+// 		echo "\n";
+// 	} else {
+// 		echo "$thing\n";
+// 	}
+// }
 
 ?>
