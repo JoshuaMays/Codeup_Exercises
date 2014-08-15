fdfc2edea9c3496cc6fbb2951f4d25e8fa80f01d
haydogsup <joshua.r.mays@gmail.com>
2014-08-15 10:09:23 -0500

    first exercise

--- foreach.php
+++ foreach.php
@@ -1,5 +1,21 @@
 <?php
 
+$things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
+foreach ($things as $thing) {
+	if (is_array($thing)) {
+		echo "\$thing is an array.\n";
+	} elseif (is_bool($thing)) {
+		echo "\$thing is a bool.\n";
+	} elseif (is_float($thing)) {
+		echo "\$thing is a float.\n";
+	} elseif (is_int($thing)) {
+		echo "\$thing is an int.\n";
+	} elseif (is_null($thing)) {
+		echo "\$thing is a null value.\n";
+	} elseif (is_string($thing)) {
+		echo "\$thing is a string.\n";
+	}
+}
 
 ?>
