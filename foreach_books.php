364cbbe3fd7286c74c16d6ec7ede3782a7a1fef3
haydogsup <joshua.r.mays@gmail.com>
2014-08-15 12:58:02 -0500

    finished exercise

--- foreach_books.php
+++ foreach_books.php
@@ -1,5 +1,37 @@
 <?php
 
+$books = array(
+    'The Hobbit' => array(
+        'published' => 1937,
+        'author' => 'J. R. R. Tolkien',
+        'pages' => 310
+    ),
+    'Game of Thrones' => array(
+        'published' => 1996,
+        'author' => 'George R. R. Martin',
+        'pages' => 835
+    ),
+    'The Catcher in the Rye' => array(
+        'published' => 1951,
+        'author' => 'J. D. Salinger',
+        'pages' => 220
+    ),
+    'A Tale of Two Cities' => array(
+        'published' => 1859,
+        'author' => 'Charles Dickens',
+        'pages' => 544
+    )
+);
 
-
+foreach ($books as $title => $bookinfo) {
+	if ($books[$title]['published'] >= 1950) {
+		echo "\n" . $title . "\n";
+		echo "##########################\n";
+		foreach ($bookinfo as $key => $metadata) {
+			echo "$key: $metadata.\n";
+		}
+			
+	}
+}
 ?>
+
