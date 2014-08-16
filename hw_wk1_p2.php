bb88a4ccd96acd8e43eb09cab2abcc3c6f9e7d9a
haydogsup <joshua.r.mays@gmail.com>
2014-08-16 13:12:17 -0500

    first commit

---
+++ hw_wk1_p2.php
@@ -0,0 +1,38 @@
+<?php
+
+$quiz = [
+	'1' => [ 
+		'question' => 'Question',
+		'answers'  => [
+			'A' => 'Answer A',
+			'B' => 'Answer B',
+			'C' => 'Answer C',
+			'D' => 'Answer D'
+		],
+		'correct_answer' = 'C'
+	], 
+	'2' => [
+		'question' => '',
+		'answers'  => [],
+		'correct_answer' = ''
+	],
+	'3' => [
+		'question' => '',
+		'answers'  => [],
+		'correct_answer' = ''
+	],
+	'4' => [
+		'question' => '',
+		'answers'  => [],
+		'correct_answer' = ''
+	],
+	'5' => [
+		'question' => '',
+		'answers'  => [],
+		'correct_answer' = ''
+	]
+
+
+];
+
+?>
