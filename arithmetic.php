2d703cba4a81cf5e1d669288bf43b8807dab9f5c
haydogsup <joshua.r.mays@gmail.com>
2014-08-18 15:34:40 -0500

    add is_numeric validation to functions

--- arithmetic.php
+++ arithmetic.php
@@ -1,39 +1,68 @@
 <?php
 
 function add($a, $b) {
-	echo $a + $b . PHP_EOL;
+	if(is_numeric($a) && is_numeric($b)) {
+		echo $a + $b . PHP_EOL;		
+	}
+	else {
+		echo "ERROR: Must pass numeric values to add()" . PHP_EOL;
+	}
+
 }
 
 function subtract($a, $b) {
-	echo $a - $b . PHP_EOL;
+	if(is_numeric($a) && is_numeric($b)) {
+		echo $a - $b . PHP_EOL;		
+	}
+	else {
+		echo "ERROR: Must pass numeric values to subtract()" . PHP_EOL;
+	}
 }
 
 function multiply($a, $b) {
-	echo $a * $b . PHP_EOL;
+	if(is_numeric($a) && is_numeric($b)) {
+		echo $a * $b . PHP_EOL;		
+	}
+	else {
+		echo "ERROR: Must pass numeric values to multiply()" . PHP_EOL;
+	}
 }
 
 function divide($a, $b) {
-	echo $a / $b . PHP_EOL;
+	if(is_numeric($a) && is_numeric($b)) {
+		echo $a / $b . PHP_EOL;		
+	}
+	else {
+		echo "ERROR: Must pass numeric values to divide()" . PHP_EOL;
+	}
 }
 
 function modulus($a, $b) {
-	echo $a % $b . PHP_EOL;
+	if(is_numeric($a) && is_numeric($b)) {
+		echo $a % $b . PHP_EOL;		
+	}
+	else {
+		echo "ERROR: Must pass numeric values to modulus()" . PHP_EOL;
+	}
 }
 
-add(1,4);
+add(1,1);
 subtract(39, 2);
-multiply(10, 5);
+multiply(10, 9);
 divide(9, 3);
 modulus(15, 3);
 
 
 $a = 50;
 $b = 12;
+$c = 100;
+$d = 3;
 
 
-add($a, $b);
+
+add($a, $c);
 subtract($a, $b);
-multiply($a, $b);
-divide($a, $b);
+multiply($c, $b);
+divide($a, $d);
 modulus($a, $b);
 ?>
