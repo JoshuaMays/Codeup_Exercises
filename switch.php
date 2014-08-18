598af8b4fefff485a73863ae89d36c267eb40fb9
haydogsup <joshua.r.mays@gmail.com>
2014-08-18 11:05:42 -0500

    commiting switch statement for days of week

---
+++ switch.php
@@ -0,0 +1,41 @@
+<?php
+
+// Set the default timezone
+date_default_timezone_set('America/Chicago');
+
+// Get Day of Week as number
+// 1 (for Monday) through 7 (for Sunday)
+$day_of_week = date('N');
+
+switch($day_of_week) {
+	case 1:
+		// Output Monday
+		echo 'Monday' . PHP_EOL;
+		break;
+	case 2:
+		// Output Tuesday
+		echo 'Tuesday' . PHP_EOL;
+		break;
+	case 3:
+		// Output Wednesday
+		echo 'Wednesday' . PHP_EOL;
+		break;
+	case 4:
+		// Output Thursday
+		echo 'Thursday' . PHP_EOL;
+		break;
+	case 5:
+		// Output Friday
+		echo 'Friday' . PHP_EOL;
+		break;
+	case 6:
+		// Output Saturday
+		echo 'Saturday' . PHP_EOL;
+		break;
+	case 7:
+		// Output Sunday
+		echo 'Sunday' . PHP_EOL;
+		break;
+}
+
+?>
