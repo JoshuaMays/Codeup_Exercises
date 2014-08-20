95db27fef2b96e9e23af821ea4f222a8dc33e58e
haydogsup <joshua.r.mays@gmail.com>
2014-08-20 15:47:49 -0500

    converted implode/explode into function

--- list_with_commas.php
+++ list_with_commas.php
@@ -1,13 +1,28 @@
 <?php
 
+function humanized_list($physicists_string) {
+	$physicists_string = explode(", ", $physicists_string);
+	$trailing_physicist = array_pop($physicists_string);
+	$physicists_string = implode(", ", $physicists_string) . ", and {$trailing_physicist}";
+	return $physicists_string;
+}
+
+
+
+
+
 $physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
 
-$famous_fake_physicists = explode(", ", $physicists_string);
+// $famous_fake_physicists = explode(", ", $physicists_string);
 
-$trailing_physicist = array_pop($famous_fake_physicists);
+// $trailing_physicist = array_pop($famous_fake_physicists);
 
-$famous_fake_physicists = implode(", ", $famous_fake_physicists) . ", and {$trailing_physicist}";
+// $famous_fake_physicists = implode(", ", $famous_fake_physicists) . ", and {$trailing_physicist}";
+
+$famous_fake_physicists = humanized_list($physicists_string);
 
 echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}." . PHP_EOL;
+
+
 
 ?>
