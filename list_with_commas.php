b58dd6387196cc22e6c82e9dc8e887c816b6aa3a
haydogsup <joshua.r.mays@gmail.com>
2014-08-20 16:06:54 -0500

    changed function to accept array

--- list_with_commas.php
+++ list_with_commas.php
@@ -1,9 +1,8 @@
 <?php
 
-function humanized_list($physicists_string) {
-	$physicists_string = explode(", ", $physicists_string);
-	$trailing_physicist = array_pop($physicists_string);
-	$physicists_string = implode(", ", $physicists_string) . ", and {$trailing_physicist}";
+function humanized_list($physicists_array) {
+	$trailing_physicist = array_pop($physicists_array);
+	$physicists_string = implode(", ", $physicists_array) . ", and {$trailing_physicist}";
 	return $physicists_string;
 }
 
@@ -12,6 +11,7 @@ function humanized_list($physicists_string) {
 
 
 $physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+$physicists_array = explode(", ", $physicists_string);
 
 // $famous_fake_physicists = explode(", ", $physicists_string);
 
@@ -19,7 +19,7 @@ $physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mal
 
 // $famous_fake_physicists = implode(", ", $famous_fake_physicists) . ", and {$trailing_physicist}";
 
-$famous_fake_physicists = humanized_list($physicists_string);
+$famous_fake_physicists = humanized_list($physicists_array);
 
 echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}." . PHP_EOL;
 
