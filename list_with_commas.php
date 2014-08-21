00c14d3ed8b58df09cf2f9d4bcf5a4f1b32a568d
haydogsup <joshua.r.mays@gmail.com>
2014-08-21 09:18:12 -0500

    changed function parameter to expect string, not array

--- list_with_commas.php
+++ list_with_commas.php
@@ -1,25 +1,28 @@
 <?php
 
 // custom function for creating humanized list with an Oxford comma from an array, unsorted version is the default.
-function humanized_list($physicists_array, $sort = false) {
+function humanized_list($string, $sort = false) {
+	// Convert string into an array.
+	$array = explode(", ", $string);	
+
 	// Check if we should sort the array first.
 	if ($sort) {
-		asort($physicists_array);
+		asort($array);
 	}
 	// Remove and save the last array value for later
-	$trailing_physicist = array_pop($physicists_array);
+	$trailing = array_pop($array);
 	// Turn array back into a string and concat the trailing value.
-	$physicists_string = implode(", ", $physicists_array) . ", and {$trailing_physicist}";
-	return $physicists_string;
+	$string = implode(", ", $array) . ", and {$trailing}";
+	return $string;
 }
 
 $physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
 
-// Convert string into an array.
-$physicists_array = explode(", ", $physicists_string);
+// // Convert string into an array.
+// $physicists_array = explode(", ", $physicists_string);
 
 // Call humanized_list and save the returned value (human-readable string).
-$famous_fake_physicists = humanized_list($physicists_array);
+$famous_fake_physicists = humanized_list($physicists_string, TRUE);
 
 echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}." . PHP_EOL;
 
