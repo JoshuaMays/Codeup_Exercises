824ba9abdb7f6963ace215a8c3520708fea5e8fb
haydogsup <joshua.r.mays@gmail.com>
2014-08-23 12:23:35 -0500

    calculates donut area

--- hw_wk2_p2.php
+++ hw_wk2_p2.php
@@ -1,5 +1,25 @@
 <?php
 
+function findArea($radius) {
+	$area = M_PI * pow($radius, 2);
+	return $area;
+}
 
+function donutArea($totalRad, $subRad) {
+	// area of outer circle
+	$outer = findArea($totalRad);
+	// area of inner circle
+	$inner = findArea($subRad);
+	
+	$areaDonut = $outer - $inner;
+	return $areaDonut;
+}
 
+$radius = 15;
+
+$area = findArea($radius);
+
+$donut = donutArea($radius,5);
+
+echo $donut;
 ?>
