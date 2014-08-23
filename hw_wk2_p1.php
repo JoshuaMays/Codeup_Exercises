2a9646ca0d6dad2cbad374de15ecaf4e7be2f347
haydogsup <joshua.r.mays@gmail.com>
2014-08-23 12:05:08 -0500

    can check num days, not for leap years tho.

--- hw_wk2_p1.php
+++ hw_wk2_p1.php
@@ -1,5 +1,15 @@
 <?php
 
 
+function daysOfMonth($month){
+	
+	$days = date('t', strtotime($month));
+	return $days;
+}
+
+$month = 'February';
+$numDays = daysOfMonth($month);
+fwrite(STDOUT, "$month has $numDays days in it.");
 
 ?>
+
