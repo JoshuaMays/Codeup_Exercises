49ce19ac350999ede4343cf9d1bc30e937cbfc87
haydogsup <joshua.r.mays@gmail.com>
2014-09-02 17:22:52 -0500

    changed n's to php_eol, changed double-quotes to single when interpolation is not needed

--- foreach.php
+++ foreach.php
@@ -5,66 +5,68 @@ $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, 
 # ECHO text as data type.
 
 // foreach ($things as $thing) {
-// 	if (is_array($thing)) {
-// 		echo "\$thing is an array.\n";
-// 	} elseif (is_bool($thing)) {
-// 		echo "\$thing is a boolean.\n";
-// 	} elseif (is_float($thing)) {
-// 		echo "\$thing is a float.\n";
-// 	} elseif (is_int($thing)) {
-// 		echo "\$thing is an int.\n";
-// 	} elseif (is_null($thing)) {
-// 		echo "\$thing is a null value.\n";
-// 	} elseif (is_string($thing)) {
-// 		echo "\$thing is a string.\n";
-// 	}
+//     if (is_array($thing)) {
+//         echo '$thing is an array.' . PHP_EOL;
+//     } elseif (is_bool($thing)) {
+//         echo '$thing is a boolean.' . PHP_EOL;
+//     } elseif (is_float($thing)) {
+//         echo '$thing is a float.' . PHP_EOL;
+//     } elseif (is_int($thing)) {
+//         echo '$thing is an int.' . PHP_EOL;
+//     } elseif (is_null($thing)) {
+//         echo '$thing is a null value.' . PHP_EOL;
+//     } elseif (is_string($thing)) {
+//         echo '$thing is a string.' . PHP_EOL;
+//     }
 // }
 
 # ECHO gettype($thing) to show data type of the values inside the $things array.
 
 // foreach ($things as $thing) {
-// 	if (is_array($thing)){
-// 		echo "\$thing is an " . gettype($thing) . " data type.\n";
-// 	} elseif (is_bool($thing)) {
-// 		echo "\$thing is a " . gettype($thing) . " data type.\n";
-// 	} elseif (is_float($thing)) {
-// 		echo "\$thing is a " . gettype($thing) . " data type.\n";
-// 	} elseif (is_int($thing)) {
-// 		echo "\$thing is an " . gettype($thing) . " data type.\n";
-// 	} elseif (is_null($thing)) {
-// 		echo "\$thing is a " . gettype($thing) . " data type.\n";
-// 	} elseif (is_string($thing)) {
-// 		echo "\$thing is a " . gettype($thing) . " data type.\n";
-// 	}
+//     if (is_array($thing)){
+//         echo '$thing is an ' . gettype($thing) . " data type." . PHP_EOL;
+//     } elseif (is_bool($thing)) {
+//         echo '$thing is a ' . gettype($thing) . " data type." . PHP_EOL;
+//     } elseif (is_float($thing)) {
+//         echo '$thing is a ' . gettype($thing) . " data type." . PHP_EOL;
+//     } elseif (is_int($thing)) {
+//         echo '$thing is an ' . gettype($thing) . " data type." . PHP_EOL;
+//     } elseif (is_null($thing)) {
+//         echo '$thing is a ' . gettype($thing) . " data type." . PHP_EOL;
+//     } elseif (is_string($thing)) {
+//         echo '$thing is a ' . gettype($thing) . " data type." . PHP_EOL;
+//     }
 // }
 
 # ECHO text to show data types for scalar values
 
-foreach ($things as $thing) {
-	if (is_scalar($thing)) {
-		if (is_float($thing)) {
-			echo "\$thing is a float.\n";
-		} elseif (is_int($thing)) {
-			echo "\$thing is an int.\n";
-		} elseif (is_string($thing)) {
-			echo "\$thing is a string.\n";
-		} elseif (is_bool($thing)) {
-			echo "\$thing is a boolean.\n";
-		}
-	}
-}
+// foreach ($things as $thing) {
+//     if (is_scalar($thing)) {
+//         if (is_float($thing)) {
+//             echo '$thing is a float.' . PHP_EOL;
+//         } elseif (is_int($thing)) {
+//             echo '$thing is an int.' . PHP_EOL;
+//         } elseif (is_string($thing)) {
+//             echo '$thing is a string.' . PHP_EOL;
+//         } elseif (is_bool($thing)) {
+//             echo '$thing is a boolean.' . PHP_EOL;
+//         }
+//     }
+// }
 
 # ECHO values, if value is an array, loop through the array and echo the stored values
 
-// foreach ($things as $thing) {
-// 	if (is_array($thing)) {
-// 		foreach ($thing as $value) {
-// 			echo "{$value} ";
-// 		}
-// 		echo "\n";
-// 	} else {
-// 		echo "$thing\n";
-// 	}
-// }
+foreach ($things as $thing) {
+    if (is_array($thing)) {
+        // If the $thing is an array, loop through array and echo the values
+        foreach ($thing as $value) {
+            echo "$value";
+        }
+        echo PHP_EOL;
+        // Just echo the value of $thing.
+    } else {
+        echo "$thing" . PHP_EOL;
+    }
+}
 
 ?>
