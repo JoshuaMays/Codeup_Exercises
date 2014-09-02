bd9dec6c0647a41c15081dc13a23167c2ad8657e
haydogsup <joshua.r.mays@gmail.com>
2014-09-02 16:52:53 -0500

    fixed comments and whitespace

--- do_while.php
+++ do_while.php
@@ -1,11 +1,11 @@
 <?php
 
-
 // Exercise 1
 $a = 0;
 do {
-	echo $a . "\n";
-	$a += 2;
+    echo $a . "\n";
+    $a += 2;
+  // Check if $a has reached 100 yet.
 } while ($a <= 100);
 
 
@@ -13,8 +13,9 @@ do {
 $a = 100;
 
 do {
-	echo $a . "\n";
-	$a -= 5;
+    echo $a . "\n";
+    $a -= 5;
+  // Check if $a has reached -10 yet.
 } while ($a >= -10);
 
 
@@ -22,8 +23,9 @@ do {
 $a = 2;
 
 do {
-	echo $a . "\n";
-	$a *= $a;
+    echo $a . "\n";
+    $a *= $a;
+  // Check if $a still less than 1000000
 } while ($a < 1000000);
 
 ?>
