111dd7ef89d0723e0a9710229ed3e4486b33c5c9
haydogsup <joshua.r.mays@gmail.com>
2014-09-02 17:10:59 -0500

    changed n's to php_eol

--- for.php
+++ for.php
@@ -14,28 +14,28 @@ $incrementer = trim(fgets(STDIN));
 // Make sure that the upper and lower bounds are numbers
 // if not, print an error message.
 while (!is_numeric($start) || !is_numeric($end)) {
-	fwrite(STDOUT,"It doesn't look like you're giving me numbers.\n" 
-				. "Please try again.\n");
-	// Reset the $start value to user input.
-	fwrite(STDOUT, "Enter a starting number: ");
-	$start = trim(fgets(STDIN));
-	// Reset the $end value to user input.
-	fwrite(STDOUT, "Now enter an ending number: ");
-	$end = trim(fgets(STDIN));
+    fwrite(STDOUT,"It doesn't look like you're giving me numbers." 
+                . PHP_EOL . "Please try again." . PHP_EOL);
+    // Reset the $start value to user input.
+    fwrite(STDOUT, "Enter a starting number: ");
+    $start = trim(fgets(STDIN));
+    // Reset the $end value to user input.
+    fwrite(STDOUT, "Now enter an ending number: ");
+    $end = trim(fgets(STDIN));
 }
 
 // Set a $incrementer to default value (1) if user input is non-numeric.
 if (!is_numeric($incrementer)) {
-	$incrementer = 1;
+    $incrementer = 1;
 }
 // Check if program should increment or decrement.
 if ($start <= $end) {
-	for ($i = $start; $i <= $end; $i+=$incrementer) {
-		fwrite(STDOUT,"$i\n");
-	}
+    for ($i = $start; $i <= $end; $i+=$incrementer) {
+        fwrite(STDOUT,"$i" . PHP_EOL);
+    }
 } else {
-	for ($i = $start; $i >= $end; $i-=$incrementer) {
-		fwrite(STDOUT, "$i\n");
+    for ($i = $start; $i >= $end; $i-=$incrementer) {
+        fwrite(STDOUT, "$i" . PHP_EOL);
     }
 }
 ?>
