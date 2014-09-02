fe5554aaddb25ae64394a1404586444f16671957
haydogsup <joshua.r.mays@gmail.com>
2014-09-02 16:47:56 -0500

    cleaned comments and spacing

--- arithmetic.php
+++ arithmetic.php
@@ -1,87 +1,89 @@
 <?php
-
+// Function adds two numbers together.
 function add($a, $b) {
-	if (is_Numeric($a) && is_Numeric($b)) {
-		$sum = $a + $b . PHP_EOL;
-		return $sum;
-	} else {
-		$errorMessage = nonNumericError();
-		return $errorMessage;
-	}
+    // Checks if arguments are numeric.
+    if (is_Numeric($a) && is_Numeric($b)) {
+        return $a + $b;
+    } else {
+        // If args are nonnumeric, call nonNumericError message.
+        return nonNumericError();
+    }
 
 }
 
 function subtract($a, $b) {
-	if (is_Numeric($a) && is_Numeric($b)) {
-		$sum = $a - $b . PHP_EOL;
-		return $sum;
-	} else {
-		$errorMessage = nonNumericError();
-		return $errorMessage;
-	}
+    // Checks if arguments are numeric.
+    if (is_Numeric($a) && is_Numeric($b)) {
+        return $a - $b;
+    } else {
+        // If args are nonnumeric, call nonNumericError message.
+        return nonNumericError();
+    }
 
 }
 
 function multiply($a, $b) {
-	if (is_Numeric($a) && is_Numeric($b)) {
-		$sum = $a * $b . PHP_EOL;
-		return $sum;
-	} else {
-		$errorMessage = nonNumericError();
-		return $errorMessage;
-	}
+    // Checks if arguments are numeric.
+    if (is_Numeric($a) && is_Numeric($b)) {
+        return $a * $b;
+    } else {
+        // If args are nonnumeric, call nonNumericError message.
+        return nonNumericError();
+    }
 
 
 }
 
 function divide($a, $b) {
-  if (is_Numeric($a) && is_Numeric($b)) {
-  		if ($b == 0) {
-  			$errorMessage = divideByZero($b);
-  			return $errorMessage;
-  		}
-  		else {
-  			$divide = $a / $b . PHP_EOL;
-  			return $divide;
-  		}
-  }
-  else {
-  		$errorMessage = nonNumericError();
-  		return $errorMessage;
-  }
+    // Checks if arguments are numeric.
+    if (is_Numeric($a) && is_Numeric($b)) {
+        // Check if divisor argument is zero
+        if ($b == 0) {
+            // Return divideByZero error message.
+            return divideByZero($b);
+        }
+        else {
+            return $a / $b;
+        }
+    }
+    else {
+        // If args are nonnumeric, call nonNumericError message.
+        return nonNumericError();
+    }
 
 }
 
 function modular($a, $b) {
-  if (is_Numeric($a) && is_Numeric($b)) {
-  		if ($b == 0) {
-  			$errorMessage = divideByZero($b);
-  			return $errorMessage;
-  		}
-  		else {
-  			$divide = $a % $b . PHP_EOL;
-  			return $divide;
-  		}
-  }
-  else {
-  		$errorMessage = nonNumericError();
-  		return $errorMessage;
-  }
+    // Checks if arguments are numeric.
+    if (is_Numeric($a) && is_Numeric($b)) {
+        // Check if divisor argument is zero
+        if ($b == 0) {
+            // Return divideByZero error message.
+            return divideByZero($b);
+        }
+        else {
+            return $a % $b;
+        }
+    }
+    else {
+        // If args are nonnumeric, call nonNumericError message.
+        return nonNumericError();
+}
 
 }
 
 function nonNumericError() {
-	return 'ERROR: One of the values is non-numeric.' . PHP_EOL;
+    return 'ERROR: One of the values is non-numeric.';
 }
 
 function divideByZero($b) {
-	return 'ERROR: Please never ever divide by zero.' . PHP_EOL;
+    return 'ERROR: Please never ever divide by zero.';
 }
 
-echo add(138, 15);
-echo subtract(131, 2);
-echo multiply(3, 101);
-echo divide(4,2);
-echo modular(4,2);
+echo add(138, 'a') . PHP_EOL;
+echo subtract(131, 'b') . PHP_EOL;
+echo multiply(3, 'c') . PHP_EOL;
+echo divide(4, '0') . PHP_EOL;
+echo modular(4,3) . PHP_EOL;
 
 ?>
