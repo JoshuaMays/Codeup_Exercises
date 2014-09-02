8fd373411bcbf74c20e2ec9afa31f6333a731f05
haydogsup <joshua.r.mays@gmail.com>
2014-09-02 16:54:28 -0500

    fixed comments and whitespace

--- do_while_exercise.php
+++ do_while_exercise.php
@@ -1,9 +1,13 @@
 <?php
 
+// Initialize $a with a value of 5.
 $a = 1;
+
 do {
+    // Print to screen, then increment $a by 1.
     echo "\$a is equal to {$a}\n";
     $a++;
+  // Now check if $a has reached 5 yet.
 } while ($a <= 5);
 
 ?>
