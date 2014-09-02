8af49217166375571e5175a3376ccd341ca75a1b
haydogsup <joshua.r.mays@gmail.com>
2014-09-02 17:08:05 -0500

    removed worst v of fizz, edited comments and whitespace

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,38 +1,27 @@
 <?php
 
-// for ($i = 1; $i <= 100; $i++) {
-// 	if($i%3 == 0) {
-// 		echo "Fizz";
-// 		if($i%5 == 0) {
-// 			echo "Buzz\n";
-// 		} else {
-// 			echo "\n";
-// 		}
-
-// 	} elseif ($i%5 == 0) {
-// 		echo "Buzz\n";
-// 	} else {
-// 		echo $i . "\n";
-// 	}	
-// }
-
-
-// for ($i = 1; $i <=100; $i++) {
-// 	if ($i%3 == 0 && $i%5 == 0) {
-// 		echo "FizzBuzz\n";
-// 	} elseif ($i%3 == 0 && $i%5 != 0) {
-// 		echo "Fizz\n";
-// 	} elseif ($i%3 != 0 && $i%5 == 0) {
-// 		echo "Buzz\n";
-// 	} else {
-// 		echo "$i\n";
-// 	}
-// }
-
 for ($i = 1; $i <= 100; $i++) {
-	$val = ($i % 3 == 0 ? "Fizz" : "") 
-		 . ($i % 5 == 0 ? "Buzz" : "");
-	echo (!$val ? $i : $val) . "\n";
+           // If iterator divisible by 3, assign Fizz to $val
+    $val = ($i % 3 == 0 ? "Fizz" : "")
+           // If iterator divisible by 5, assign Buzz to string $val -- concat Fizz and Buzz if both.
+         . ($i % 5 == 0 ? "Buzz" : "");
+          // if $val string is empty, echo the iterator, else echo the string.
+    echo (!$val ? $i : $val) . "\n";
 }
 
+// for ($i = 1; $i <=100; $i++) {
+//     // If iterator is divisible by 3 AND 5, echo FizzBuzz
+//     if ($i%3 == 0 && $i%5 == 0) {
+//         echo "FizzBuzz\n";
+//       // If iterator is only divisible by 3, echo Fizz
+//     } elseif ($i%3 == 0 && $i%5 != 0) {
+//         echo "Fizz\n";
+//       // If iterator is only divisible by 5, echo Buzz
+//     } elseif ($i%3 != 0 && $i%5 == 0) {
+//         echo "Buzz\n";
+//       // If iterator is not divisible by either 3 or 5, echo just the iterator.
+//     } else {
+//         echo "$i\n";
+//     }
+// }
 ?>
