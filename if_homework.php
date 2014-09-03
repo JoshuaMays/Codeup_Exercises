0762a6ffb99376bc96fb51ff324ff0d87f6b35cb
haydogsup <joshua.r.mays@gmail.com>
2014-09-03 17:57:40 -0500

    make it perty.

--- if_homework.php
+++ if_homework.php
@@ -3,17 +3,19 @@
 $hungry = false;
 $food_available = true;
 
+// Check if both hungry and food is available.
 if ($hungry && $food_available) {
-	echo "We are fed and good to go.\n";
+	echo "We are fed and good to go." . PHP_EOL;
 }
 
 $have_money = false;
 
+// Check if hungry and there is food, or if have money.
 if ($hungry && $food_available || $have_money) {
-	echo "You should go eat.\n";
+	echo "You should go eat." . PHP_EOL;
 
 } else {
-	echo "Not hungry, but thank you!\n";
+	echo "Not hungry, but thank you!" . PHP_EOL;
 }
 
 ?>
