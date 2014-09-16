91583eb593b8361030169783b23d6b6c0b0a2033
haydogsup <joshua.r.mays@gmail.com>
2014-09-16 17:59:03 -0500

    commenting functions

--- hw_wk2_p2.php
+++ hw_wk2_p2.php
@@ -1,18 +1,18 @@
 <?php
-
+// Function to find the area of a circle
 function findArea($radius) {
-	$area = M_PI * pow($radius, 2);
-	return $area;
+    $area = M_PI * pow($radius, 2);
+    return $area;
 }
-
+// Function to find the area of a donut
 function donutArea($totalRad, $subRad) {
-	// area of outer circle
-	$outer = findArea($totalRad);
-	// area of inner circle
-	$inner = findArea($subRad);
-	
-	$areaDonut = $outer - $inner;
-	return $areaDonut;
+    // area of outer circle
+    $outer = findArea($totalRad);
+    // area of inner circle
+    $inner = findArea($subRad);
+    
+    $areaDonut = $outer - $inner;
+    return $areaDonut;
 }
 
 $radius = 15;
