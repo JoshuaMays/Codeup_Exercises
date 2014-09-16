e498f9c017cf949273ad5654cfba2b7fb758d99f
haydogsup <joshua.r.mays@gmail.com>
2014-09-16 18:03:42 -0500

    tabs to spaces

--- if_homework.php
+++ if_homework.php
@@ -5,17 +5,17 @@ $food_available = true;
 
 // Check if both hungry and food is available.
 if ($hungry && $food_available) {
-	echo "We are fed and good to go." . PHP_EOL;
+    echo "We are fed and good to go." . PHP_EOL;
 }
 
 $have_money = false;
 
 // Check if hungry and there is food, or if have money.
 if ($hungry && $food_available || $have_money) {
-	echo "You should go eat." . PHP_EOL;
+    echo "You should go eat." . PHP_EOL;
 
 } else {
-	echo "Not hungry, but thank you!" . PHP_EOL;
+    echo "Not hungry, but thank you!" . PHP_EOL;
 }
 
 ?>
