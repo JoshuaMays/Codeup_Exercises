74ebae0931da6c6bb12744de269326aa00218968
haydogsup <joshua.r.mays@gmail.com>
2014-09-16 17:44:35 -0500

    removing old solutions

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,27 +1,13 @@
 <?php
 
+// Solution to FizzBuzz programming exercise
 for ($i = 1; $i <= 100; $i++) {
            // If iterator divisible by 3, assign Fizz to $val
     $val = ($i % 3 == 0 ? "Fizz" : "")
            // If iterator divisible by 5, assign Buzz to string $val -- concat Fizz and Buzz if both.
          . ($i % 5 == 0 ? "Buzz" : "");
           // if $val string is empty, echo the iterator, else echo the string.
-    echo (!$val ? $i : $val) . "\n";
+    echo (!$val ? $i : $val) . PHP_EOL;
 }
 
-// for ($i = 1; $i <=100; $i++) {
-//     // If iterator is divisible by 3 AND 5, echo FizzBuzz
-//     if ($i%3 == 0 && $i%5 == 0) {
-//         echo "FizzBuzz\n";
-//       // If iterator is only divisible by 3, echo Fizz
-//     } elseif ($i%3 == 0 && $i%5 != 0) {
-//         echo "Fizz\n";
-//       // If iterator is only divisible by 5, echo Buzz
-//     } elseif ($i%3 != 0 && $i%5 == 0) {
-//         echo "Buzz\n";
-//       // If iterator is not divisible by either 3 or 5, echo just the iterator.
-//     } else {
-//         echo "$i\n";
-//     }
-// }
 ?>
