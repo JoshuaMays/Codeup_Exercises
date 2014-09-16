66976d84551ff08ee4e7d8a98c0df84b5789097b
haydogsup <joshua.r.mays@gmail.com>
2014-09-16 18:02:51 -0500

    tabs to spaces

--- while.php
+++ while.php
@@ -4,9 +4,9 @@ $test = 5;
 
 // Keep looping until $test reaches 15.
 while ($test <= 15) {
-	echo $test . PHP_EOL;
-	$test++;
-	usleep(750000);
+    echo $test . PHP_EOL;
+    $test++;
+    usleep(750000);
 }
 
 ?>
