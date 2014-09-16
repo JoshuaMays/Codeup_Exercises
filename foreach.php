e524a8ee53bd0eb56822c176c80925e9786a2950
haydogsup <joshua.r.mays@gmail.com>
2014-09-16 17:47:01 -0500

    uncommenting the older examples of using foreach loops

--- foreach.php
+++ foreach.php
@@ -2,59 +2,59 @@
 
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
-# ECHO text as data type.
+// ECHO text as data type.
 
-// foreach ($things as $thing) {
-//     if (is_array($thing)) {
-//         echo '$thing is an array.' . PHP_EOL;
-//     } elseif (is_bool($thing)) {
-//         echo '$thing is a boolean.' . PHP_EOL;
-//     } elseif (is_float($thing)) {
-//         echo '$thing is a float.' . PHP_EOL;
-//     } elseif (is_int($thing)) {
-//         echo '$thing is an int.' . PHP_EOL;
-//     } elseif (is_null($thing)) {
-//         echo '$thing is a null value.' . PHP_EOL;
-//     } elseif (is_string($thing)) {
-//         echo '$thing is a string.' . PHP_EOL;
-//     }
-// }
+foreach ($things as $thing) {
+    if (is_array($thing)) {
+        echo '$thing is an array.' . PHP_EOL;
+    } elseif (is_bool($thing)) {
+        echo '$thing is a boolean.' . PHP_EOL;
+    } elseif (is_float($thing)) {
+        echo '$thing is a float.' . PHP_EOL;
+    } elseif (is_int($thing)) {
+        echo '$thing is an int.' . PHP_EOL;
+    } elseif (is_null($thing)) {
+        echo '$thing is a null value.' . PHP_EOL;
+    } elseif (is_string($thing)) {
+        echo '$thing is a string.' . PHP_EOL;
+    }
+}
 
-# ECHO gettype($thing) to show data type of the values inside the $things array.
+// ECHO gettype($thing) to show data type of the values inside the $things array.
 
-// foreach ($things as $thing) {
-//     if (is_array($thing)){
-//         echo '$thing is an ' . gettype($thing) . " data type." . PHP_EOL;
-//     } elseif (is_bool($thing)) {
-//         echo '$thing is a ' . gettype($thing) . " data type." . PHP_EOL;
-//     } elseif (is_float($thing)) {
-//         echo '$thing is a ' . gettype($thing) . " data type." . PHP_EOL;
-//     } elseif (is_int($thing)) {
-//         echo '$thing is an ' . gettype($thing) . " data type." . PHP_EOL;
-//     } elseif (is_null($thing)) {
-//         echo '$thing is a ' . gettype($thing) . " data type." . PHP_EOL;
-//     } elseif (is_string($thing)) {
-//         echo '$thing is a ' . gettype($thing) . " data type." . PHP_EOL;
-//     }
-// }
+foreach ($things as $thing) {
+    if (is_array($thing)){
+        echo '$thing is an ' . gettype($thing) . " data type." . PHP_EOL;
+    } elseif (is_bool($thing)) {
+        echo '$thing is a ' . gettype($thing) . " data type." . PHP_EOL;
+    } elseif (is_float($thing)) {
+        echo '$thing is a ' . gettype($thing) . " data type." . PHP_EOL;
+    } elseif (is_int($thing)) {
+        echo '$thing is an ' . gettype($thing) . " data type." . PHP_EOL;
+    } elseif (is_null($thing)) {
+        echo '$thing is a ' . gettype($thing) . " data type." . PHP_EOL;
+    } elseif (is_string($thing)) {
+        echo '$thing is a ' . gettype($thing) . " data type." . PHP_EOL;
+    }
+}
 
-# ECHO text to show data types for scalar values
+// ECHO text to show data types for scalar values
 
-// foreach ($things as $thing) {
-//     if (is_scalar($thing)) {
-//         if (is_float($thing)) {
-//             echo '$thing is a float.' . PHP_EOL;
-//         } elseif (is_int($thing)) {
-//             echo '$thing is an int.' . PHP_EOL;
-//         } elseif (is_string($thing)) {
-//             echo '$thing is a string.' . PHP_EOL;
-//         } elseif (is_bool($thing)) {
-//             echo '$thing is a boolean.' . PHP_EOL;
-//         }
-//     }
-// }
+foreach ($things as $thing) {
+    if (is_scalar($thing)) {
+        if (is_float($thing)) {
+            echo '$thing is a float.' . PHP_EOL;
+        } elseif (is_int($thing)) {
+            echo '$thing is an int.' . PHP_EOL;
+        } elseif (is_string($thing)) {
+            echo '$thing is a string.' . PHP_EOL;
+        } elseif (is_bool($thing)) {
+            echo '$thing is a boolean.' . PHP_EOL;
+        }
+    }
+}
 
-# ECHO values, if value is an array, loop through the array and echo the stored values
+// ECHO values, if value is an array, loop through the array and echo the stored values
 
 foreach ($things as $thing) {
     if (is_array($thing)) {
