19eb93fc1b4e74e0f44fda121f68ddc50c83d951
haydogsup <joshua.r.mays@gmail.com>
2014-09-16 17:56:01 -0500

    removing testing work

--- internal_functions.php
+++ internal_functions.php
@@ -7,46 +7,28 @@ $serialArray = serialize($array);
 $unserialArray = unserialize($serialArray);
 
 function amISet($check) {
-	unset($check);
-	if (isset($check)) {
-		$result = "SET";
-		return $result;
-	}
-	else {
-		$result = "EMPTY";
-		return $result;
-	}
+    unset($check);
+    if (isset($check)) {
+        $result = "SET";
+        return $result;
+    }
+    else {
+        $result = "EMPTY";
+        return $result;
+    }
 }
-
-// function amIEmpty($check) {
-// 	if (empty($check)) {
-// 		$result = "EMPTY";
-// 		return $result;
-// 	}
-// 	else {
-// 		$result = "NOT EMPTY";
-// 		return $result;
-// 	}
-// }
-
 
 $checked = amISet($nothing);
 echo '$checked is ' . $checked . '.' . PHP_EOL;
 
-// $checked = amIEmpty($nothing);
-// echo '$checked is ' . $checked . '.' . PHP_EOL;
-
 $checked = amISet($something);
 echo '$checked is ' . $checked . '.' . PHP_EOL;
 
-// $checked = amIEmpty($something);
-// echo '$checked is ' . $checked . '.' . PHP_EOL;
+$checked = amISet($serialArray);
+echo '$checked is ' . $checked . '.' . PHP_EOL;
 
-// $checked = amISet($serialArray);
-// echo '$checked is ' . $checked . '.' . PHP_EOL;
-
-// $checked = amISet($unserialArray);
-// echo '$checked is ' . $checked . '.' . PHP_EOL;
+$checked = amISet($unserialArray);
+echo '$checked is ' . $checked . '.' . PHP_EOL;
 
 
 ?>
